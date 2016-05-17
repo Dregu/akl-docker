db67d1e2eba137b3571b84eb68ce720ecef565ba
Dregu <ville.nousiainen@gmail.com>
2016-05-17 09:50:49 +0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,23 +1,98 @@
 #!/bin/bash
 
-HOSTNAME=AKL\#$1
-RCONPASSWORD=foo
-PASSWORD=bar
+I=1
+GSLT=
+HOSTNAME=CS:GO
+RCONPASSWORD=
+PASSWORD=
 TICKRATE=128
 MAP=de_dust2
-MAPGROUP=random_classic
+MAPGROUP=mg_active
 MAXPLAYERS=16
 GAMEMODE=1
 GAMETYPE=0
-TVENABLE=1
-
-PORTOFFSET=$((($1-1)*100))
+[[ $I -gt 0 ]] || I=1
+PORTOFFSET=$((($I-1)*100))
 SERVERPORT=$((27015+$PORTOFFSET))
 TVPORT=$((27020+$PORTOFFSET))
 CLIENTPORT=$((27005+$PORTOFFSET))
+DOCKERNAME=
+ENTRYPOINT=/home/cs/start
 
-docker run -dti --name akl$1 -h akl$1 -p $SERVERPORT:27015/tcp -p $SERVERPORT:27015/udp -p $TVPORT:27020/udp -p $CLIENTPORT:27005/udp --entrypoint /home/cs/serverfiles/srcds_run dregu/csgo -game csgo -usercon -strictportbind -ip 0.0.0.0 -port 27015 +clientport 27005 +tv_port 27020 -tickrate $TICKRATE +sv_setsteamaccount $2 +map $MAP +servercfgfile csgo-server.cfg -maxplayers_override $MAXPLAYERS +mapgroup $MAPGROUP +game_mode $GAMEMODE +game_type $GAMETYPE
-docker exec akl$1 sed -i "/hostname \"/s/\"\([^\\\"]*\)\"/\"$HOSTNAME\"/" serverfiles/csgo/cfg/csgo-server.cfg
-docker exec akl$1 sed -i "/rcon_password \"/s/\"\([^\\\"]*\)\"/\"$RCONPASSWORD\"/" serverfiles/csgo/cfg/csgo-server.cfg
-docker exec akl$1 sed -i "/sv_password \"/s/\"\([^\\\"]*\)\"/\"$PASSWORD\"/" serverfiles/csgo/cfg/csgo-server.cfg
-docker exec akl$1 sed -ie "\$atv_enable \"$TVENABLE\"" serverfiles/csgo/cfg/csgo-server.cfg
+while getopts "i:g:h:r:p:t:m:M:o:y:P:I:e:H:" i; do
+  case $i in
+    i)
+      I=$OPTARG
+      DOCKERNAME="--name cs$I -h cs$I"
+      HOSTNAME="CS:GO #$I"
+      PORTOFFSET=$((($I-1)*100))
+      SERVERPORT=$((27015+$PORTOFFSET))
+      TVPORT=$((27020+$PORTOFFSET))
+      CLIENTPORT=$((27005+$PORTOFFSET))
+      ;;
+    g)
+      GSLT="$OPTARG"
+      ;;
+    h)
+      HOSTNAME="$OPTARG"
+      ;;
+    r)
+      RCONPASSWORD="$OPTARG"
+      ;;
+    p)
+      PASSWORD="$OPTARG"
+      ;;
+    t)
+      TICKRATE="$OPTARG"
+      ;;
+    m)
+      MAP="$OPTARG"
+      ;;
+    M)
+      MAXPLAYERS="$OPTARG"
+      ;;
+    o)
+      GAMEMODE="$OPTARG"      
+      ;;
+    y)
+      GAMETYPE="$OPTARG"
+      ;;
+    P)
+      SERVERPORT="$OPTARG"
+      TVPORT=$(($SERVERPORT+5))
+      CLIENTPORT=$(($SERVERPORT-10))
+      ;;
+    I)
+      SERVERPORT="$OPTARG:$SERVERPORT"
+      TVPORT="$OPTARG:$TVPORT"
+      CLIENTPORT="$OPTARG:$CLIENTPORT"
+      ;;
+    e)
+      ENTRYPOINT="$OPTARG"
+      ;;
+    H)
+      DOCKERNAME="--name $OPTARG -h $OPTARG"
+      ;;
+    \?)
+      echo 'Usage: ./server.sh
+  -i index
+  -g gslt
+  -l sv_lan
+  -h hostname
+  -r rcon_password
+  -p password
+  -t tickrate
+  -m map
+  -M maxplayers
+  -o game_mode
+  -y game_type
+  -P port
+  -I ip
+  -e docker entrypoint'
+      exit 1
+      ;;
+  esac
+done
+
+docker build -t "dregu/csgo:latest" .
+docker run -dti $DOCKERNAME -p $SERVERPORT:27015/tcp -p $SERVERPORT:27015/udp -p $TVPORT:27020/udp -p $CLIENTPORT:27005/udp -e TICKRATE="$TICKRATE" -e GSLT="$GSLT" -e MAP="$MAP" -e MAXPLAYERS="$MAXPLAYERS" -e MAPGROUP="$MAPGROUP" -e GAMEMODE="$GAMEMODE" -e GAMETYPE="$GAMETYPE" -e HOSTNAME="$HOSTNAME" -e RCONPASSWORD="$RCONPASSWORD" -e PASSWORD="$PASSWORD" --entrypoint "$ENTRYPOINT" dregu/csgo
